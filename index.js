9d774996abdc213cf3cc948b24fb36768838cf30
natia <nati.shonia37@gmail.com>
2022-05-25 16:43:51 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import { compareAsc, format } from 'date-fns'
+
+console.log(format(new Date(2014, 1, 11), 'yyyy-MM-dd'))
+
+
+const dates = [
+    new Date(1995, 6, 2),
+    new Date(1987, 1, 11),
+    new Date(1989, 6, 10),
+]
+console.log(dates.sort(compareAsc))
